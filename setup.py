e1087f0c891728f1cb72ddad6008152d57a5c8b4
Gustavo Rosa <gth.rosa@uol.com.br>
2020-08-18 11:32:09 -0300

    Initial files commit.

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(name='transpopt',
+      version='1.0.0',
+      description='Optimal Transport Transforms',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Gustavo Rosa',
+      author_email='gustavo.rosa@unesp.br',
+      url='https://github.com/gugarosa/transpopt',
+      license='GPL-3.0',
+      install_requires=['coverage>=5.2.1',
+                        'pylint>=2.5.3',
+                        'pytest>=6.0.1'
+                        ],
+      extras_require={
+          'tests': ['coverage',
+                    'pytest',
+                    'pytest-pep8',
+                    ],
+      },
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Education',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=find_packages())
